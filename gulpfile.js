c4d11b82194aec1db57dad8f05fd8debeb9a0b7d
Karim Mohamed <komicho1996@gmail.com>
2019-02-11 00:47:04 +0200

    Append js files and fix assets

--- gulpfile.js
+++ gulpfile.js
@@ -4,14 +4,14 @@ const gulp = require('gulp'),
     nodemon = require('gulp-nodemon');
 
 gulp.task('assets', function() {
-    return gulp.src(['./src/assets/**/*'])
+    return gulp.src(['./src/assets/**/*.*'])
     .pipe(gulp.dest('./dist/public/'));
 });
 
-gulp.task('build', function() {
+gulp.task('buildHtml', function() {
     return gulp.src("./src/pages/*.ejs")
     .pipe(ejs({ msg: 'Hello Gulp!'}, {}, { ext: '.html' }))
-    .pipe(gulp.dest('./dist'))
+    .pipe(gulp.dest('./dist'));
 });
 
 
@@ -19,6 +19,13 @@ gulp.task('allCss', function() {
     return gulp.src([
         './src/assets/css/index.css'
     ]).pipe(concat('all.css'))
+    .pipe(gulp.dest('./src/assets/'));
+});
+
+gulp.task('allJs', function() {
+    return gulp.src([
+        './src/assets/js/app.js'
+    ]).pipe(concat('all.js'))
     .pipe(gulp.dest('./src/assets/'));
 });
 
@@ -31,8 +38,9 @@ gulp.task('serve', function (done) {
     })
 });
 
-gulp.task('default', ['serve', 'allCss']);
+gulp.task('default', ['serve', 'allCss', 'allJs']);
 
-gulp.task('build', ['build', 'allCss', 'assets']);
+gulp.task('build', ['buildHtml', 'allCss', 'allJs', 'assets']);
 
 gulp.watch('./src/assets/**/*.css', ['allCss']);
+gulp.watch('./src/assets/**/*.js', ['allJs']);
